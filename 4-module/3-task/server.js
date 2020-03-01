4aaf2d58c87dc1c2b0544a5459f5375f02ede9e1
kurpachsv <kurpachsv@gmail.com>
2020-03-01 17:27:18 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,17 +1,38 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
+const checkPath = (pathname) => {
+  return pathname.indexOf('/') === -1 && pathname.indexOf('..') === -1;
+};
+
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
-
+      if (!checkPath(pathname)) {
+        res.statusCode = 400;
+        return res.end();
+      }
+      fs.stat(filepath, (err, stats) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            return res.end();
+          }
+          throw err;
+        }
+        fs.unlink(filepath, (err) => {
+          if (err) throw err;
+          res.statusCode = 200;
+          res.end();
+        });
+      });
       break;
 
     default:
