964986b192efc6a6c233288e6a762f1783837421
kurpachsv <kurpachsv@gmail.com>
2020-03-01 22:52:54 +0300

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,10 +8,28 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const noop = () => {};
+let users = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  users.push(ctx.res);
+  ctx.res.on('close', () => {
+    users.splice(users.indexOf(ctx.res), 1);
+  });
+  await new Promise(noop);
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+  console.log(`publish '${message}'`);
+  if (message) {
+    users.forEach((res) => {
+      res.statusCode = 200;
+      res.end(message);
+    });
+    users = [];
+  }
+  ctx.body = 'ok';
 });
 
 app.use(router.routes());
